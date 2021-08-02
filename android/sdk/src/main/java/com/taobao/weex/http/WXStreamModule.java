9bc7bee39f1be9dcc63c1c97c5f3a26cb0574599
Huiying Jiang <jhy221188@alibaba-inc.com>
2021-08-02 14:41:22 +0800

    Revert "fix:add PathInfo"
    This reverts commit 79e6af9ce8e776e7a2912e97908f59378e7f8029.

--- android/sdk/src/main/java/com/taobao/weex/http/WXStreamModule.java
+++ android/sdk/src/main/java/com/taobao/weex/http/WXStreamModule.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
+ * 
  *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -221,7 +221,7 @@ public class WXStreamModule extends WXModule {
 
     private String parseReferer(String url) {
         try {
-            JSONArray whiteList = JSONArray.parseArray("[\"sellerId\", \"userId\", \"shopId\", \"pageId\",\"pathInfo\"]");
+            JSONArray whiteList = JSONArray.parseArray("[\"sellerId\", \"userId\", \"shopId\", \"pageId\"]");
             IWXConfigAdapter adapter = WXSDKManager.getInstance().getWxConfigAdapter();
             if (null != adapter) {
                 String config = adapter.getConfig("wx-x-page-url", "ParamsWhiteList", null);
